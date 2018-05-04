2b016ecc71ffe91392cd92f5d88b2278f9ee7e75
Adrian Jablonski <jablonski.adrian1@gmail.com>
2018-05-03 20:54:11 -0500

    Added decrement 1, increment 5, and decrement 5

--- Counter_Redux_React/redux-counter/src/components/Counter.js
+++ Counter_Redux_React/redux-counter/src/components/Counter.js
@@ -12,10 +12,10 @@ class Counter extends Component {
         return (
             <div>
                 <CounterOutput counterLabel = {this.props.ctr} />
-                <CounterControl onButtonPush = {this.props.onIncrementCounter} title="increment +1" />
-                <CounterControl title="decrement -1" />
-                <CounterControl title="increment +5" />
-                <CounterControl title="decrement -5" />
+                <CounterControl onButtonPush = {this.props.onIncrementCounter1} title="increment +1" />
+                <CounterControl onButtonPush = {this.props.onDecrementCounter1} title="decrement -1" />
+                <CounterControl onButtonPush = {this.props.onIncrementCounter5} title="increment +5" />
+                <CounterControl onButtonPush = {this.props.onDecrementCounter5} title="decrement -5" />
             </div>
         )
     }
@@ -28,7 +28,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter : () => dispatch({type : 'INC_COUNTER'})
+        onIncrementCounter1 : () => dispatch({type : 'INC_COUNTER1'}),
+        onDecrementCounter1 : () => dispatch({type : 'DEC_COUNTER1'}),
+        onIncrementCounter5 : () => dispatch({type : 'INC_COUNTER5'}),
+        onDecrementCounter5 : () => dispatch({type : 'DEC_COUNTER5'})
     }
 }
 
--- Counter_Redux_React/redux-counter/src/store/reducer.js
+++ Counter_Redux_React/redux-counter/src/store/reducer.js
@@ -3,10 +3,31 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    return {
-        ...state,
-        counter : state.counter + 1
+    if (action.type == 'INC_COUNTER1') {
+        return {
+            ...state,
+            counter : state.counter + 1
+        }
     }
+    else if (action.type == 'DEC_COUNTER1') {
+        return {
+            ...state,
+            counter : state.counter - 1
+        }
+    }
+    else if (action.type == 'INC_COUNTER5') {
+        return {
+            ...state,
+            counter : state.counter + 5
+        }
+    }
+    else if (action.type == 'DEC_COUNTER5') {
+        return {
+            ...state,
+            counter : state.counter - 5
+        }
+    }
+    return state
 }
 
 export default reducer
